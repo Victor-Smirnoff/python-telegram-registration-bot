fc3665d572239e8c875dd55a13c5655bada0c49d
Victor Smirnov <csatom@yandex.ru>
2024-03-14 14:07:39 +0300

    feat: added create_user method

--- crud_users.py
+++ crud_users.py
@@ -1,4 +1,4 @@
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, Result
 from model import User
@@ -12,7 +12,7 @@ class CRUDUser:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def find_by_id(self, telegram_user_id: int) -> User | None:
+    async def find_user_by_telegram_user_id(self, telegram_user_id: int) -> User | None:
         """
         Метод возвращает найденный объект класса User если он найден в БД, иначе объект None
         :param telegram_user_id: айди пользователя
@@ -29,4 +29,30 @@ class CRUDUser:
                     return None
         except SQLAlchemyError:
             return None
-    
+
+    async def create_user(
+            self,
+            user_id: int,
+            password: str,
+    ) -> User | None:
+        """
+        Метод записывает нового пользователя в БД
+        :param user_id: телеграм айди пользователя
+        :param password: строка - хэшированное значение пароля
+        :return: объект класса User | None
+        """
+
+        if not all((user_id, password)):
+            return None
+
+        try:
+            new_user = User(user_id=user_id, password=password)
+            self.session.add(new_user)
+            try:
+                await self.session.commit()
+                await self.session.refresh(new_user)
+                return new_user
+            except IntegrityError:
+                return None
+        except SQLAlchemyError:
+            return None
