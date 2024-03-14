ac2787680c64d0cbcc9bb7653d60a68277cf8a90
Victor Smirnov <csatom@yandex.ru>
2024-03-14 13:02:47 +0300

    fix: added password to table Users in model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
 
 from database import Base
 
@@ -7,3 +8,4 @@ class User(Base):
     __tablename__ = "users"
 
     user_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(length=128), nullable=False)
